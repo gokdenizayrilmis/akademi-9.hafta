a59a92fbe7a1262aa0c0dafecb3dfd6e01a2d079
gokdeniz <ayrilmis.gokdeniz@gmail.com>
2025-01-18 00:44:12 +0300

    program.cs dosyası oluşturuldu.

---
+++ program.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Globalization;
+
+namespace ConsoleApp
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+
+        }
+    }
+}
+
